753681c2a9750e337c5bb0ac2caf1c3ae1b0467e
Yesbol <esbolvideo@gmail.com>
2022-05-24 17:59:21 +0500

    Add files via upload

---
+++ ropestar/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+		async Codelevels_Event8_Act7(runtime, localVars)
+		{
+			coolmathCallLevelStart(runtime.globalVars.currentLevel);
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
