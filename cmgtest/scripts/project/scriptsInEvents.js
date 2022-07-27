8722284d1bfdb2ce4454c1297a574b951aaccf06
Yesbol <esbolvideo@gmail.com>
2022-07-28 00:29:48 +0500

    Add files via upload

---
+++ cmgtest/scripts/project/scriptsInEvents.js
@@ -0,0 +1,60 @@
+function coolmathCallStart()
+{
+if (typeof parent.cmgGameEvent === "function") {
+try {
+parent.cmgGameEvent("start");
+} catch (e) {}
+}
+console.log("game start event");
+}
+function coolmathCallLevelStart(level)
+{
+if (typeof parent.cmgGameEvent === "function") {
+try
+{parent.cmgGameEvent("start", String(level));
+} catch (e) {}
+}
+console.log("level start " + level);
+}
+function coolmathCallLevelRestart(level)
+{
+if (typeof parent.cmgGameEvent === "function") {
+try {
+parent.cmgGameEvent("replay", String(level));
+} catch (e) {}
+}
+console.log("level restart " + level);
+}
+
+
+const scriptsInEvents = {
+
+		async Eventgame_Event41_Act2(runtime, localVars)
+		{
+			coolmathCallLevelStart(runtime.globalVars.currentLevel);
+		},
+
+		async Eventgame_Event42_Act3(runtime, localVars)
+		{
+			coolmathCallLevelStart(runtime.globalVars.currentLevel);
+		},
+
+		async Eventmenu_Event28_Act3(runtime, localVars)
+		{
+			coolmathCallLevelStart(runtime.globalVars.currentLevel);
+		},
+
+		async Eventmenu_Event29_Act3(runtime, localVars)
+		{
+			coolmathCallLevelStart(runtime.globalVars.currentLevel);
+		},
+
+		async Eventpreload_Event2_Act2(runtime, localVars)
+		{
+			coolmathCallStart();
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
