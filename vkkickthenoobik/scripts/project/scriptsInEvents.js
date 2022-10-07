a153784e94f53823efc22ba0412a0b93a591420d
Yesbol <esbolvideo@gmail.com>
2022-10-07 11:33:03 +0500

    Add files via upload

---
+++ vkkickthenoobik/scripts/project/scriptsInEvents.js
@@ -0,0 +1,37 @@
+
+
+
+const scriptsInEvents = {
+
+	async GameEs_Event31_Act7(runtime, localVars)
+	{
+		platformProvider.showRewarded()
+	},
+
+	async GameEs_Event48_Act7(runtime, localVars)
+	{
+		let options = {
+			message:localVars.zzz,
+			link: 'https://vk.com/app51441061'
+		}
+		platformProvider.share(options)
+	},
+
+	async StartEs_Event8_Act6(runtime, localVars)
+	{
+		platformProvider.addToFavorites()
+	},
+
+	async StartEs_Event9_Act6(runtime, localVars)
+	{
+		let options = {
+			message:'Я играю в Kick the Noobik!!! Присоединяйся к игре!',
+			link: 'https://vk.com/app51441061'
+		}
+		platformProvider.share(options)
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
